96625377a828d8ea942bcf7acaeb95f13804a1b1
Martin Quintana <m.quintana@buenosaires.gob.ar>
2019-11-27 16:24:17 -0300

    first steps in fullstackopen react course

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
 
 export default App;
